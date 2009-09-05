e0bcee82b60f3c6d3baec5f2265b3f32d8b48ab5
David Wright <dcwuser@users.noreply.github.com>
2009-09-05 01:30:25 +0000

    Working 3j and CG code.

--- Core/Core/Spin.cs
+++ Core/Core/Spin.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+
 
 using Meta.Numerics.Functions;
 
 namespace Meta.Numerics {
 
-#if FUTURE
-
+    /// <summary>
+    /// Represents a spinor.
+    /// </summary>
     public struct Spin {
 
         // construction
 
+        /// <summary>
+        /// Instantiates a new spinor.
+        /// </summary>
+        /// <param name="j">The spin, which must be an integer or half-integer.</param>
         public Spin (double j) {
 
             // no negative (or too large) spins
@@ -32,14 +39,24 @@ namespace Meta.Numerics {
 
         // accessors
 
+        /// <summary>
+        /// Gets the spin of the spinor.
+        /// </summary>
+        public double J {
+            get {
+                return (twoJ / 2.0);
+            }
+        }
+
         internal int TwoJ {
             get {
-                return (TwoJ);
+                return (twoJ);
             }
         }
 
         // casting
 
+        /*
         public static implicit operator Spin (double j) {
             return (new Spin(j));
         }
@@ -47,46 +64,131 @@ namespace Meta.Numerics {
         public static implicit operator double (Spin s) {
             return (s.TwoJ / 2.0);
         }
+        */
+
+        // pre-defined
+
+        /// <summary>
+        /// Gets a spin-0 spinor.
+        /// </summary>
+        public static Spin SpinZero {
+            get {
+                return (new Spin(0.0));
+            }
+        }
+
+        /// <summary>
+        /// Gets is spin-1/2 spinor.
+        /// </summary>
+        public static Spin SpinOneHalf {
+            get {
+                return (new Spin(0.5));
+            }
+        }
+
+        /// <summary>
+        /// Gets a spin-1 spinor.
+        /// </summary>
+        public static Spin SpinOne {
+            get {
+                return (new Spin(1.0));
+            }
+        }
+
+        // irrep data
+
+        /// <summary>
+        /// Gets the dimension of the spinor.
+        /// </summary>
+        public int Dimension {
+            get {
+                return (twoJ + 1);
+            }
+        }
+
+        /// <summary>
+        /// Returns the set of spinor states.
+        /// </summary>
+        /// <returns>An array of spin states that spans the spinor subspace.</returns>
+        public SpinState[] States () {
+            List<SpinState> states = new List<SpinState>();
+            for (int i = -twoJ; i <= twoJ; i += 2) {
+                states.Add(new SpinState(twoJ / 2.0, i / 2.0));
+            }
+            //for (int twoM = -TwoJ; twoM <= 0; twoM = twoM + 2) {
+            //    states.Add(new SpinState(twoJ / 2.0, twoM / 2.0));
+            //}
+            return (states.ToArray());
+        }
+
+        // equality
+
+        private static bool Equals (Spin a, Spin b) {
+            return(a.TwoJ == b.TwoJ);
+        }
+
+        public static bool operator == (Spin a, Spin b) {
+            return (Equals(a, b));
+        }
+
+        public static bool operator != (Spin a, Spin b) {
+            return (!Equals(a, b));
+        }
 
     }
 
+    /// <summary>
+    /// Represents the state of a spinor.
+    /// </summary>
     public struct SpinState {
 
-        public SpinState (double j, double m) {
+        public SpinState (double j, double m) : this(new Spin(j), m)  { }
 
-            // no negative (or too large) spins
-            double tj = 2.0 * j;
-            if ((tj < 0.0) || (tj > Int32.MaxValue)) throw new ArgumentOutOfRangeException("j");
+        public SpinState (Spin s, double m) {
 
-            // spin must be integer or half-integer
-            double tjt = Math.Truncate(tj);
-            if (tjt != tj) throw new ArgumentOutOfRangeException("j");
+            spin = s;
 
-            // store 2*j
-            twoJ = (int) tjt;
-
-            // M must be an integer
+            // 2M must be an integer
             double tm = 2.0 * m;
             double tmt = Math.Truncate(tm);
             if (tmt != tm) throw new ArgumentOutOfRangeException("m");
 
+            int tmti = (int) tmt;
+
             twoM = (int) tmt;
-
             // -J <= M <= J
-            if (Math.Abs(twoM) > Math.Abs(twoJ)) throw new ArgumentOutOfRangeException("m");
-
-            // half-integer J requires half-integer M; integer J requires integer M
-            if ((twoJ % 2) != Math.Abs(twoM % 2)) throw new ArgumentOutOfRangeException("m");
+            if (Math.Abs(tmti) > s.TwoJ) throw new ArgumentOutOfRangeException("m");
             
+            // half-integer J requires half-integer M; integer J requires integer M
+            if ((s.TwoJ % 2) != Math.Abs(twoM % 2)) throw new ArgumentOutOfRangeException("m");
+
         }
 
-        private int twoJ;
+        private Spin spin;
 
         private int twoM;
 
+        /// <summary>
+        /// Gets the spin value of the spin state.
+        /// </summary>
+        public double J {
+            get {
+                return (spin.J);
+            }
+        }
+
+        /// <summary>
+        /// Gets the magnetic substate value of the spin state.
+        /// </summary>
+        public double M {
+            get {
+                return (twoM / 2.0);
+            }
+        }
+
         internal int TwoJ {
             get {
-                return (twoJ);
+                return (spin.TwoJ);
             }
         }
 
@@ -98,19 +200,28 @@ namespace Meta.Numerics {
 
         internal int JPlusM {
             get {
-                return ((twoJ + twoM) / 2);
+                return ((spin.TwoJ + twoM) / 2);
             }
         }
 
         internal int JMinusM {
             get {
-                return ((twoJ - twoM) / 2);
+                return ((spin.TwoJ - twoM) / 2);
             }
         }
 
         internal SpinState Invert () {
             twoM = -twoM;
             return (this);
+        }
+
+        /// <summary>
+        /// Gets the spinor representation to which the spin state belongs.
+        /// </summary>
+        public Spin Representation {
+            get {
+                return (spin);
+            }
         }
 
     }
@@ -153,8 +264,12 @@ namespace Meta.Numerics {
         public static double ThreeJ (SpinState s1, SpinState s2, SpinState s3) {
 
             // require that Js satisfy triangle inequality
-            if (s3.TwoJ < Math.Abs(s1.TwoJ - s2.TwoJ)) throw new InvalidOperationException();
-            if (s3.TwoJ > (s1.TwoJ + s2.TwoJ)) throw new InvalidOperationException();
+            if (s3.TwoJ < Math.Abs(s1.TwoJ - s2.TwoJ)) return (0.0);
+            if (s3.TwoJ > (s1.TwoJ + s2.TwoJ)) return (0.0);
+
+            // require that the parity of the spins agree
+            // for example, spin-1 and spin-2 cannot combine to spin 3/2, even though 1 < 3/2 < 3
+            if ((s1.TwoJ + s2.TwoJ + s3.TwoJ) % 2 != 0) return (0.0);
 
             // require that Ms add to zero
             if ((s1.TwoM + s2.TwoM + s3.TwoM) != 0) return(0.0);
@@ -195,28 +310,22 @@ namespace Meta.Numerics {
             } else if (s1.TwoJ == 2) {
                 // J1 = 1
                 return (s * ThreeJ_OneJ(s1, s2, s3));
-            } else if (s1.TwoJ == 3) {
-                // J1 = 3/2
-                return (s * ThreeJ_Any(s1, s2, s3));
-                // replace this with special case!
-            } else if ((s1.TwoJ + s2.TwoJ) == s3.TwoJ) {
-                // J1+J2=J3, maximum coupling
-                return (s * ThreeJ_Any(s1, s2, s3));
-                //return (ThreeJ_MaxJ(s1, s2));
-                // should this really be special cased? there is only one term in sum.
             } else {
                 // arbitrary Js
                 if ((s1.TwoM == 0) && (s2.TwoM == 0) && (s3.TwoM == 0)) {
                     // special case of all M's zero
                     return (s * ThreeJ_ZeroM_Int(s1, s2, s3));
-                    // also for other low Ms, because there are many terms to compute these cases
                 } else {
                     // general case
-                    return (s * ThreeJ_Any(s1, s2, s3));
+                    return (s * ThreeJ_ShultenGordon_RecurseJ(s1, s2, s3));
                 }
             }
 
-            throw new NotImplementedException();
+            // other possible special cases include:
+            // J1 = 3/2, J1 = 2
+            // Ms = 1/2 -1/2 0, 1/2 1/2 1, 1 -1 0 and permutations
+            // maximum coupling J3 = J1 + J2, minimum coupling J3 = |J1 - J2|
+
         }
 
         // exchange two references
@@ -297,6 +406,7 @@ namespace Meta.Numerics {
 
         }
 
+#if FUTURE
 
         // ( 3/2  J2  J3 )
         // ( M1   M2  M3 )
@@ -413,6 +523,8 @@ namespace Meta.Numerics {
             return (f * g);
         }
 
+#endif
+
         // ( J1  J2  J3 )
         // ( 0   0   0  )
         private static double ThreeJ_ZeroM_Int (SpinState s1, SpinState s2, SpinState s3) {
@@ -437,7 +549,7 @@ namespace Meta.Numerics {
                 int B = j - s2.TwoJ;
                 int C = j - s3.TwoJ;
 
-                Console.WriteLine("A={0} B={1} C={2}", A, B, C);
+                //Console.WriteLine("A={0} B={1} C={2}", A, B, C);
 
                 // note that A >= B >= C, because of the ordering of js; therefore we eliminate factors involving C, which are largest
                 // note also that A, B, and C are even, because j is even and we subtract even numbers from j to get them
@@ -468,7 +580,7 @@ namespace Meta.Numerics {
                 for (int i = 1; i <= hC; i++) {
                     g = g * (hC + i) * (hA + hB + i) * (hA + hB + i) / (A + B + i) / (A + B + hC + i) / i;
                 }
-                Console.WriteLine("g={0}", g);
+                //Console.WriteLine("g={0}", g);
                 g = Math.Sqrt(g);
 
                 if ((j/2) % 2 != 0) f = -f;
@@ -478,6 +590,8 @@ namespace Meta.Numerics {
 
 
         }
+
+#if FUTURE
 
         // ( J1  J2  J3 )
         // ( 0   0   0  )
@@ -539,6 +653,171 @@ namespace Meta.Numerics {
             }
 
         }
+#endif
+
+        // method described by Shulten & Gordon in J. Math. Phys. 16 (1975) 1961
+        // uses the recurrsion
+        //  j3 A_(j3+1) ( j1 j2 j3+1 ) + B_j3 ( j1 j2 j3 ) + (j3+1) A_j3 ( j1 j2 j3-1 )
+        //              ( m1 m2  m3  )        ( m1 m2 m3 )               ( m1 m2  m3  )
+        // plus the normalization condition
+        // \sum_j3 (2 j3 + 1) ( j1 j2 j3 )^2 = 1
+        //                    ( m1 m2 m3 )
+
+        // we recurse on j3 because, for j1 <= j2 <= j3, the typical number of recursion steps
+        // will be 2 * j1, the lowest of the three possible spins to recurse on
+
+        private static double ThreeJ_ShultenGordon_RecurseJ (SpinState s1, SpinState s2, SpinState s3) {
+
+            // find the range of j
+            int tj_min = Math.Abs(s1.TwoJ - s2.TwoJ);
+            if (tj_min < Math.Abs(s3.TwoM)) tj_min = Math.Abs(s3.TwoM);
+            int tj_max = s1.TwoJ + s2.TwoJ;
+
+            // find the midpoint of the range
+            int tj_mid = (tj_min + tj_max) / 2;
+            if ((tj_mid % 2) != (tj_max % 2)) tj_mid++;
+
+            // variables to store recurrence coefficients and (unscaled) 3j values
+            double ap, a0, b0;
+            double cm, c0, cp;
+            int tj;
+
+            // a variable to store the sought value
+            double c = 0.0;
+
+            // iterate in from the right
+            double NR = 0.0;
+            cp = 0.0;
+            ap = 0.0;
+            c0 = 0.5 / tj_max;
+            if (((s1.TwoJ - s2.TwoJ - s3.TwoM) / 2) % 2 != 0) c0 = -c0; // sign convention
+            tj = tj_max;
+            while (true) {
+
+                // keep track of the normalization constant
+                NR += (tj + 1) * c0 * c0;
+
+                // remember the (unscaled) value of the desired 3j symbol
+                if (tj == s3.TwoJ) {
+                    c = c0;
+                }
+
+                // check whether we have reached the center
+                if (tj <= tj_mid) {
+                    if (Math.Abs(c0) <= Math.Pow(2.0, -40.0) * Math.Abs(cp)) {
+                        // if the middle value is zero, the left and right values cannot be matached
+                        // (acutally, the numbers will be two random tiny numbers, and their ratio will
+                        // be a random number of order one); in this case, go down one more step
+                        tj_mid -= 2;
+                    } else {
+                        break;
+                    }
+                }
+
+
+                // compute required coefficients
+                b0 = ShultenGodron_B(s1.TwoJ, s2.TwoJ, tj, s1.TwoM, s2.TwoM, s3.TwoM);
+                a0 = ShultenGordon_A(s1.TwoJ, s2.TwoJ, tj, s3.TwoM);
+
+                // use recursion to compute the tj-2 symbol
+                cm = (-b0 * c0 - (tj / 2.0) * ap * cp) / ((tj + 2) / 2.0) / a0;
+
+                //Console.WriteLine("{0}: {1} {2} + {3} {4} + {5} {6}", tj, ap, cp, b0, c0, a0, cm);
+
+                // prepare for the next cycle
+                cp = c0;
+                c0 = cm;
+                ap = a0;
+                tj -= 2;
+
+            }
+
+            // remember the (unscaled) middle value
+            double c1 = c0;
+
+            // iterate in from the left
+            double NL = 0.0;
+            cm = 0.0;
+            a0 = 0.0;
+            c0 = 0.5 / tj_max;
+            tj = tj_min;
+
+            // j_min = 0 iff j1 = j2 and m3 = 0
+            // in this case for j3=0, b0 = 0 and all three coefficients vanish, so the recursion is indeterminate
+            // in this case we do the first recursion step "by hand" using relationships
+
+            if (tj == 0) {
+                NL += c0 * c0;
+                cm = c0;
+                c0 = s1.TwoM / Math.Sqrt(s1.TwoJ * (s1.TwoJ + 2)) * cm;
+                a0 = ShultenGordon_A(s1.TwoJ, s2.TwoJ, 2, 0);
+                tj = 2;
+            }
+
+            while (true) {
+
+                // check whether we have reached the center
+                if (tj >= tj_mid) {
+                    break;
+                }
+                
+                // notice that we terminate before checking whether we are at the desired value,
+                // or adding to the normalization constant; this is different from the previous loop
+                // we do this so that the middle element does not double contribute to the normalization
+                // and because if the middle element is desired, it has already been stored from the previous loop
+
+                // remember the desired (unscaled) value
+                if (tj == s3.TwoJ) {
+                    c = c0;
+                }
+
+                // add current value to normalization
+                NL += (tj + 1) * c0 * c0;
+
+                // check for end
+                if (tj >= tj_max) break;
+
+                // compute required coeficients
+                b0 = ShultenGodron_B(s1.TwoJ, s2.TwoJ, tj, s1.TwoM, s2.TwoM, s3.TwoM);
+                ap = ShultenGordon_A(s1.TwoJ, s2.TwoJ, tj + 2, s3.TwoM);
+
+                // use recursion relation to compute tj+2
+                cp = (-b0 * c0 - ((tj + 2) / 2.0) * a0 * cm) / (tj / 2.0) / ap;
+
+                //Console.WriteLine("{0}: {1} {2} + {3} {4} + {5} {6}", tj, ap, cp, b0, c0, a0, cm);
+
+                // prepare for next iteration
+                cm = c0;
+                c0 = cp;
+                a0 = ap;
+                tj += 2;
+
+            }
+
+            // match in the middle
+            double r = c0 / c1;
+            NL = NL / (r * r);
+            if (s3.TwoJ < tj_mid) c = c / r;
+
+            // normalize
+            double N = NL + NR;
+            c = c / Math.Sqrt(N);
+
+            return (c);
+
+        }
+
+        private static double ShultenGordon_A (int tj1, int tj2, int tj3, int tm3) {
+            double f1 = (tj3 - (tj1 - tj2)) * (tj3 + (tj1 - tj2)) / 4.0;
+            double f2 = ((tj1 + tj2 + 2) - tj3) * ((tj1 + tj2 + 2) + tj3) / 4.0;
+            double f3 = (tj3 + tm3) * (tj3 - tm3) / 4.0;
+            return (Math.Sqrt(f1 * f2 * f3));
+        }
+
+        private static double ShultenGodron_B (int tj1, int tj2, int tj3, int tm1, int tm2, int tm3) {
+            double f = (tm3 * (tj1 * (tj1 + 2) - tj2 * (tj2 + 2)) + (tm1 - tm2) * tj3 * (tj3 + 2)) / 8.0;
+            return (-(tj3 + 1) * f);
+        }
 
         // SixJ
 
@@ -548,6 +827,6 @@ namespace Meta.Numerics {
 
     }
 
-#endif
+//#endif
 
 }

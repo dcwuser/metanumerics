c109b60343cf1dde009916bc27d36873d3486e01
David Wright <dcwuser@live.com>
2017-04-03 23:02:28 -0700

    Add ref doc to Discrete interval, rename file

--- Numerics/Core/DIscreteInterval.cs
+++ Numerics/Core/DIscreteInterval.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Meta.Numerics {
+
+    /// <summary>
+    /// Represents an interval on the integers.
+    /// </summary>
     public struct DiscreteInterval {
 
         public DiscreteInterval (int min, int max) {
@@ -16,12 +20,18 @@ namespace Meta.Numerics {
 
         private readonly int max;
 
+        /// <summary>
+        /// The minimum value on the interval.
+        /// </summary>
         public int LeftEndpoint {
             get {
                 return (min);
             }
         }
 
+        /// <summary>
+        /// The maximum value on the interval.
+        /// </summary>
         public int RightEndpoint {
             get {
                 return (max);
